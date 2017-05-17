485c09f3c5e9e40efdc19a5a549ede9bfc5b0d77
ali alshattawi <ali.alshattawi@gmail.com>
2017-05-17 22:48:15 +0000

    add confrmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable#,:confirmable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable,:confirmable
   belongs_to :fitness
   belongs_to :course
 
